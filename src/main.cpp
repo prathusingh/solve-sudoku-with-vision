e8695fc1ca3362132c01372ce9d4544ee372ad84
Prathu Singh <prathusingh21@gmail.com>
2020-02-02 21:41:54 -0500

    Corrected image path

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+//#include <highgui.h>
 
 using namespace cv;
 
 int main(int argc, char const *argv[])
 {
     std::cout << "Solve sudoku using vision" << std::endl;
+    Mat sudoku = imread("../sudoku.jpg", 1);
+    imshow("Sudoku", sudoku);
+    waitKey(0);
     return 0;
 }

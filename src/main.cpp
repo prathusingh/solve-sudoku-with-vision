17f95aa56c92f1c7ab7e70d8e61b602f64ac958a
Prathu Singh <prathusingh21@gmail.com>
2020-02-02 22:39:08 -0500

    Threshold and dilated the image

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,16 @@ using namespace cv;
 int main(int argc, char const *argv[])
 {
     std::cout << "Solve sudoku using vision" << std::endl;
-    Mat sudoku = imread("../sudoku.jpg", 1);
+    Mat sudoku = imread("../sudoku.jpg", CV_8UC1);
+    Mat outerBox = Mat(sudoku.size(), CV_8UC1);
+
     GaussianBlur(sudoku, sudoku, Size(11, 11), 0);
-    imshow("Sudoku", sudoku);
+    adaptiveThreshold(sudoku, outerBox, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 5, 2);
+    bitwise_not(outerBox, outerBox);
+    Mat kernel = (Mat_<uchar>(3, 3) << 0, 1, 0, 1, 1, 1, 0, 1, 0);
+    dilate(outerBox, outerBox, kernel);
+    //imshow("Sudoku", sudoku);
+    imshow("Outer box", outerBox);
     waitKey(0);
     return 0;
 }

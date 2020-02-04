2e5deae07f94fd0cdecf189975c5b9d203322e05
Prathu Singh <prathusingh21@gmail.com>
2020-02-03 22:12:17 -0500

    Trying different blur function

--- src/main.cpp
+++ src/main.cpp
@@ -26,13 +26,14 @@ int main(int argc, char const *argv[])
     Mat sudoku = imread("../sudoku.jpg", CV_8UC1);
     Mat outerBox = Mat(sudoku.size(), CV_8UC1);
 
-    GaussianBlur(sudoku, sudoku, Size(11, 11), 0);
-    adaptiveThreshold(sudoku, outerBox, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 5, 2);
+    GaussianBlur(sudoku, sudoku, Size(45, 45), 0);
+    imshow("image", sudoku);
+    //adaptiveThreshold(sudoku, outerBox, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 5, 2);
 
-    bitwise_not(outerBox, outerBox);
+    //bitwise_not(outerBox, outerBox);
 
-    Mat kernel = (Mat_<uchar>(3, 3) << 0, 1, 0, 1, 1, 1, 0, 1, 0);
-    dilate(outerBox, outerBox, kernel);
+    //Mat kernel = (Mat_<uchar>(3, 3) << 0, 1, 0, 1, 1, 1, 0, 1, 0);
+    //dilate(outerBox, outerBox, kernel);
 
     // find the biggest bounding box
     int count = 0;
@@ -40,46 +41,46 @@ int main(int argc, char const *argv[])
 
     Point maxPt;
 
-    for (int y = 0; y < outerBox.size().height; y++)
-    {
-        uchar *row = outerBox.ptr(y);
-        for (int x = 0; x < outerBox.size().width; x++)
-        {
-            if (row[x] >= 128)
-            {
-                int area = floodFill(outerBox, Point(x, y), CV_RGB(0, 0, 64));
-                if (area > max)
-                {
-                    maxPt = Point(x, y);
-                    max = area;
-                }
-            }
-        }
-    }
+    // for (int y = 0; y < outerBox.size().height; y++)
+    // {
+    //     uchar *row = outerBox.ptr(y);
+    //     for (int x = 0; x < outerBox.size().width; x++)
+    //     {
+    //         if (row[x] >= 128)
+    //         {
+    //             int area = floodFill(outerBox, Point(x, y), CV_RGB(0, 0, 64));
+    //             if (area > max)
+    //             {
+    //                 maxPt = Point(x, y);
+    //                 max = area;
+    //             }
+    //         }
+    //     }
+    // }
 
-    floodFill(outerBox, maxPt, CV_RGB(255, 255, 255));
+    // floodFill(outerBox, maxPt, CV_RGB(255, 255, 255));
 
-    for (int y = 0; y < outerBox.size().height; y++)
-    {
-        uchar *row = outerBox.ptr(y);
-        for (int x = 0; x < outerBox.size().width; x++)
-        {
-            if (row[x] == 64 && x != maxPt.x && y != maxPt.y)
-            {
-                int area = floodFill(outerBox, Point(x, y), CV_RGB(0, 0, 0));
-            }
-        }
-    }
+    // for (int y = 0; y < outerBox.size().height; y++)
+    // {
+    //     uchar *row = outerBox.ptr(y);
+    //     for (int x = 0; x < outerBox.size().width; x++)
+    //     {
+    //         if (row[x] == 64 && x != maxPt.x && y != maxPt.y)
+    //         {
+    //             int area = floodFill(outerBox, Point(x, y), CV_RGB(0, 0, 0));
+    //         }
+    //     }
+    // }
 
-    erode(outerBox, outerBox, kernel);
-    imshow("thresholded", outerBox);
+    // erode(outerBox, outerBox, kernel);
+    // imshow("thresholded", outerBox);
 
-    std::vector<Vec2f> lines;
-    HoughLines(outerBox, lines, 1, CV_PI / 180, 200);
-    for (int i = 0; i < lines.size(); i++)
-    {
-        drawLine(lines[i], outerBox, CV_RGB(0, 0, 128));
-    }
+    // std::vector<Vec2f> lines;
+    // HoughLines(outerBox, lines, 1, CV_PI / 180, 200);
+    // for (int i = 0; i < lines.size(); i++)
+    // {
+    //     drawLine(lines[i], outerBox, CV_RGB(0, 0, 128));
+    // }
 
     waitKey(0);
     return 0;

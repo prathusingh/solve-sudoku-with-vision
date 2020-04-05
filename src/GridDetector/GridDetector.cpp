e59a5a6736929034a60caf6c3e8d56873934af10
Prathu Singh <prathusingh21@gmail.com>
2020-04-04 23:59:12 -0400

    Added comments

--- src/GridDetector/GridDetector.cpp
+++ src/GridDetector/GridDetector.cpp
@@ -11,6 +11,7 @@ void GridDetector::findBiggestBlob(Mat box)
 
     Point maxPt;
 
+    // find the bounding box area
     for (int y = 0; y < box.size().height; y++)
     {
         uchar *row = box.ptr(y);
@@ -30,8 +31,10 @@ void GridDetector::findBiggestBlob(Mat box)
         }
     }
 
+    // flood fill the outer box
     floodFill(box, maxPt, CV_RGB(255, 255, 255));
 
+    // Turn other blobs black, since bounding box is captured
     for (int y = 0; y < box.size().height; y++)
     {
         uchar *row = box.ptr(y);
